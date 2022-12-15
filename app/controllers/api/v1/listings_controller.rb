bfe48b36bca3aaebf964f590f9abc5ca4c7f6034
Anne Ly <anne.ly59@gmail.com>
2022-12-15 14:31:37 +0100

    not finished

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -1,5 +1,36 @@
-class Api::V1::ListinfsController < Api::V1::BaseController
+class Api::V1::ListingsController < Api::V1::BaseController
+  before_action :set_listing, only: %i[show update delete]
+
   def index
     @listings = Listing.all
+    render json: @listings
   end
+
+  def show
+    render json: @listing
+  end
+
+  def create
+    @listing = Listing.new(params_listing)
+    render json: @listing
+  end
+
+  def update
+    @listing = Listing.update(params_listing)
+    render json: @listing
+  end
+
+  def destroy
+    @listing.destroy
+  end
+
+private
+
+def set_listing
+  @listing = Listing.find(params[:id])
+end
+
+def params_listing
+  params.require(:listing).permit(:num_rooms)
+end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,7 @@
 class Listing < ApplicationRecord
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :missions, dependent: :destroy
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
 
   validates :num_rooms, presence: true
 end
